d9756fb8e2f052ae7aee27b06031c01b1427ece2
sintaung <sintaung8643@gmail.com>
2023-03-19 02:16:17 +0630

    First commit

---
+++ Player.gd
@@ -0,0 +1,35 @@
+extends KinematicBody2D
+
+
+const UP = Vector2(0,-1)
+const GRAVITY = 20
+const MAXFALLSPEED = 200
+const MAXSPEED = 80
+const JUMPFORCE = 300
+const ACCEL = 10
+var motion = Vector2()
+
+func _ready():
+	pass 
+	
+
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+	
+	motion.x = clamp(motion.x,-MAXSPEED,MAXSPEED)
+	
+	if Input.is_action_pressed("right"):
+		motion.x += ACCEL
+	elif Input.is_action_pressed("left"):
+		motion.x -= ACCEL
+	else:
+		motion.x = lerp(motion.x,0,0.2)
+	
+	if is_on_floor():	
+		if Input.is_action_just_pressed("jump"):
+			motion.y = -JUMPFORCE
+				
+	motion = move_and_slide(motion,UP)
